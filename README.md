27cda78d2bbf972d13b32a018f08aa5e1c1c8e97
Hojjat Abedie <hojjatabedie@gmail.com>
2018-12-24 16:31:55 +0330

    initialize feader project

---
+++ README.md
@@ -0,0 +1,12 @@
+# Feader
+
+Create a test application to handle multi module applicatonn with maven
+
+```shell
+mvn clean package
+```
+or
+```shell
+./mvn clean package
+```
+
---
+++ repository/src/main/java/com/glinboy/feader/repository/App.java
@@ -0,0 +1,13 @@
+package com.glinboy.feader.repository;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
