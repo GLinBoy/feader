2e42ee7bd0bd5069500f4e03bc9919a47d5333b0
Hojjat Abedie <hojjatabedie@gmail.com>
2019-02-09 00:13:09 +0330

    add document for ModelTest

--- model/src/test/java/com/glinboy/feader/model/ModelTest.java
+++ model/src/test/java/com/glinboy/feader/model/ModelTest.java
@@ -6,11 +6,37 @@ import java.lang.reflect.Field;
 
 import org.junit.jupiter.api.Test;
 
-public abstract class ModelTest<T> {
+/**
+ * This is base class for model test and every model tests must extends this class
+ * 
+ * @author GLinBoy
+ *
+ * @param <T>
+ */
+public abstract class ModelTest<T extends BaseModel> {
 	
+	
+	/**
+	 * This method must return current object created from T
+	 * @return object with type T
+	 */
 	public abstract T getCurrentModelObject();
+	
+	
+	/**
+	 * This method must return class of T
+	 * <br/>
+	 * e.g.: <i>return Foo.class</i>
+	 * @return class of type T
+	 */
 	public abstract Class<T> getCurrentModelClass();
 
+	/**
+	 * This method check all fields checked in tests
+	 * 
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 */
 	@Test
 	public 	void checkFeedFields() throws IllegalArgumentException, IllegalAccessException {
 		Field[] allFields = getCurrentModelClass().getDeclaredFields();
