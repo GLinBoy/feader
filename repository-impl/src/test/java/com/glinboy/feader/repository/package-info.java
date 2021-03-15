fd2669fa7d79e383390c566203580e54af37c6fe
GLinBoy <hojjatabedie@gmail.com>
2021-03-15 13:24:41 +0330

    🐛 FIX: Rmoved modularity of maven

--- repository-impl/src/test/java/com/glinboy/feader/repository/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.glinboy.feader.repository;
---
+++ src/main/java/com/glinboy/feader/FeaderApplication.java
@@ -0,0 +1,14 @@
+package com.glinboy.feader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+public class FeaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeaderApplication.class, args);
+	}
+
+}
