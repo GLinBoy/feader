b7e953fbf16bba8745cc3bec6293eb9e98fcb99a
GLinBoy <hojjatabedie@gmail.com>
2021-08-20 20:26:26 +0430

    👌 IMPROVE: Made AuditableDTO serializable
    resolved #38

--- src/main/java/com/glinboy/feader/service/dto/AuditableDTO.java
+++ src/main/java/com/glinboy/feader/service/dto/AuditableDTO.java
@@ -1,12 +1,13 @@
 package com.glinboy.feader.service.dto;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import lombok.Data;
 
 @Data
-public abstract class AuditableDTO {
-
+public abstract class AuditableDTO implements Serializable {
+  private static final long serialVersionUID = -2468866069782427130L;
   private String createdBy;
   private String editedBy;
   private Date createdOn;
