79bc2ff7b88992f9decd082265a305fdb4657672
GLinBoy <hojjatabedie@gmail.com>
2021-08-21 19:04:13 +0430

    👌 IMPROVE: Added serialVersionUID
    resolved #43

--- src/main/java/com/glinboy/feader/service/dto/CategoryDTO.java
+++ src/main/java/com/glinboy/feader/service/dto/CategoryDTO.java
@@ -7,6 +7,7 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper=true)
 public class CategoryDTO extends BaseDTO {
 
+  private static final long serialVersionUID = -4171512062504996945L;
   private String name;
   private Short weight;
 
