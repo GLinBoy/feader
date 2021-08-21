c05a42a0fe586e496a9e3580d59cfe758c0ea9f6
GLinBoy <hojjatabedie@gmail.com>
2021-08-21 18:57:07 +0430

    👌 IMPROVE: Made BaseDTO serializable
    resolved #40

--- src/main/java/com/glinboy/feader/service/dto/BaseDTO.java
+++ src/main/java/com/glinboy/feader/service/dto/BaseDTO.java
@@ -1,10 +1,13 @@
 package com.glinboy.feader.service.dto;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public abstract class BaseDTO {
+public abstract class BaseDTO implements Serializable {
 
+  private static final long serialVersionUID = -4865116964261227697L;
   private Long id;
 
 }
