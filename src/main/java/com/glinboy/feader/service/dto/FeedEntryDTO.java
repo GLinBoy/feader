34838ffc6177584c460c2757907c6bb098cfe9fd
GLinBoy <hojjatabedie@gmail.com>
2021-08-23 21:09:58 +0430

    👌 IMPROVE: Made FeedEntryDTO serializable
    resolved #47

--- src/main/java/com/glinboy/feader/service/dto/FeedEntryDTO.java
+++ src/main/java/com/glinboy/feader/service/dto/FeedEntryDTO.java
@@ -7,10 +7,12 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 public class FeedEntryDTO extends BaseDTO {
 
+  private static final long serialVersionUID = -3214656891391553373L;
   private String title;
   private String description;
   private String link;
   private String author;
   private String guid;
   private Long feedId;
+
 }
