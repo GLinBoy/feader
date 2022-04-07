c20bcedf4b76ab496a2f12ed218b5858028e0003
GLinBoy <hojjatabedie@gmail.com>
2022-04-07 20:24:38 +0200

    📦 NEW: Added unit test file for FeederGenericServiceImpl

---
+++ src/test/java/com/glinboy/feader/service/impl/FeederGenericServiceImplTest.java
@@ -0,0 +1,54 @@
+package com.glinboy.feader.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FeederGenericServiceImplTest {
+
+	@Test
+	void testFeederGenericServiceImpl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSingleById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteSingleById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteSingleByReference() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteAll() {
+		fail("Not yet implemented");
+	}
+
+}
