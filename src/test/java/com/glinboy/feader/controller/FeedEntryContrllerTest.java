67419f8bbfcc90da41982807e434e3d4bd88afb3
GLinBoy <hojjatabedie@gmail.com>
2022-04-07 20:26:43 +0200

    📦 NEW: Added unit tests file for FeedEntryContrller

---
+++ src/test/java/com/glinboy/feader/controller/FeedEntryContrllerTest.java
@@ -0,0 +1,14 @@
+package com.glinboy.feader.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FeedEntryContrllerTest {
+
+	@Test
+	void testViewEntry() {
+		fail("Not yet implemented");
+	}
+
+}
