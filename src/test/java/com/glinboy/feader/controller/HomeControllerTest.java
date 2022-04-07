09fb532683aa226dcbdee7d84a8a880171bc5328
GLinBoy <hojjatabedie@gmail.com>
2022-04-07 20:27:29 +0200

    📦 NEW: Added unit tests file for HomeController

---
+++ src/test/java/com/glinboy/feader/controller/HomeControllerTest.java
@@ -0,0 +1,19 @@
+package com.glinboy.feader.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HomeControllerTest {
+
+	@Test
+	void testGetHomePage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAddFragment() {
+		fail("Not yet implemented");
+	}
+
+}
