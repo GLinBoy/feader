9b1206343792ef4d5ba6e4c28ae8bfbbd6c37d71
GLinBoy <hojjatabedie@gmail.com>
2022-04-07 20:26:01 +0200

    📦 NEW: Added unit tests file for FeedController

---
+++ src/test/java/com/glinboy/feader/controller/FeedControllerTest.java
@@ -0,0 +1,14 @@
+package com.glinboy.feader.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FeedControllerTest {
+
+	@Test
+	void testViewFeed() {
+		fail("Not yet implemented");
+	}
+
+}
